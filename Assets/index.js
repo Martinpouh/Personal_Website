2e7ad107c3ee897b04d4d9db0f695d0f51f4ebc2
Martinpouh <martinchege2015@gmail.com>
2022-06-05 23:02:03 +0300

    update the latest changes

---
+++ Assets/index.js
@@ -0,0 +1,14 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Frontend Developer", "Volunteer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Frontend Developer", "Volunteer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
