ad659c09f77eb27a2b12c4095d90ee7603100402
Martinpouh <martinchege2015@gmail.com>
2022-06-05 23:41:08 +0300

    add an event listener on iid connect

--- Assets/index.js
+++ Assets/index.js
@@ -1,14 +1,5 @@
-// typing text animation script
-var typed = new Typed(".typing", {
-    strings: ["Frontend Developer", "Volunteer", "Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-
-var typed = new Typed(".typing-2", {
-    strings: ["Frontend Developer", "Volunteer", "Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
+function mouseover() {
+    document.getElementById("connect").addEventListener("click", function() {
+        alert("Hello World!");
+    });
+}
